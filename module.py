59e79edefc855f833cd8d09a5a6f660265a46ec3
Jorge Viera <Jorge5psgt@gmail.com>
2023-12-10 15:52:21 -0500

    first commit

--- module.py
+++ module.py
@@ -1,2 +1,7 @@
-def printName(name):
-    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+# def printName(name):
+#     print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+def homeSq():
+    num_width = (input('Whats the width?'))
+    # num_height = int(input('Whats the height?'))
+    num_length = (input('Whats the length?'))
+print(int('num_width') * int('num_length'))
